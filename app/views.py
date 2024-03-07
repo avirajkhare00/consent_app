8444f92b5b5dad72e93981f0105725a886b19ec4
avirajkhare00 <avirajkhare00@gmail.com>
2024-03-07 19:39:30 +0530

    [Aviraj] Using system font

--- app/views.py
+++ app/views.py
@@ -49,9 +49,9 @@ def post(request):
         pdf.setTitle(documentTitle)
 
         # registering a external font in python
-        pdfmetrics.registerFont(
-            TTFont('abc', 'DroidSansMono.ttf')
-        )
+        # pdfmetrics.registerFont(
+        #     TTFont('abc', 'DroidSansMono.ttf')
+        # )
 
         # creating the title by setting it's font
         # and putting it on the canvas
